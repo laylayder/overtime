924759828cacf59a102ec13cc741e0d82d1d6219
laylayder <111984219+laylayder@users.noreply.github.com>
2023-03-23 12:11:18 +0800

    Update otimejs.js

--- otimejs.js
+++ otimejs.js
@@ -33,15 +33,13 @@ button.addEventListener('click',()=>{
             const overtimePay = parseInt(hourSalary*2*secondHr)+parseInt ((hourSalary*parseInt(overtime-2))*thridHr.toFixed(3));
             result.innerHTML="奴工的醫藥費"+overtimePay;
         }else{
-           if(overtime > 8 && overtime <=12){
-                const overtimePay = parseInt(hourSalary*2*secondHr)+parseInt(hourSalary*6*thridHr)+parseInt(hourSalary*parseInt (overtime-8))*eighthHr;
+            if(overtime > 8 && overtime <=12){
+                const overtimePay = parseInt(hourSalary*2*secondHr)+parseInt((hourSalary*6*thridHr)+parseInt(hourSalary*parseInt (overtime-8))*eighthHr.toFixed(3));
                 result.innerHTML="奴工的醫藥費"+overtimePay;
         }else{
             if(overtime>12){
-                result.innerHTML="您已經違反勞基法,請打1999或1955";
-            }
-        }
-    }}
-       
-});
+                    result.innerHTML="您已經違反勞基法,請打1999或1955"; } } 
+                }} 
+                
+                }); 
 
